ae4a2349deef8cbed07ebfaa99faf4c29cc2c786
Simon Moe <simmoe@gmail.com>
2018-08-09 11:52:38 +0200

    p5 audio in eksempel til web

--- Processing/fang_en_lyd_p5/sketch.js
+++ Processing/fang_en_lyd_p5/sketch.js
@@ -3,50 +3,60 @@ var mic, recorder, soundFile;
 var state = 0; // mousePress will increment from Record, to Stop, to Play
 
 function setup() {
-  createCanvas(400,400);
-  background(200);
-  fill(0);
-  text('Enable mic and click the mouse to begin recording', 20, 20);
+    createCanvas(400, 400);
+    fill(20, 200, 0);
+    noStroke();
+    ellipse(200, 200, 300, 300);
+    fill(0);
+    textSize(14);
+    text('Enable mic and click the mouse to begin recording', 20, 380);
 
-  // create an audio in
-  mic = new p5.AudioIn();
+    // create an audio in
+    mic = new p5.AudioIn();
 
-  // users must manually enable their browser microphone for recording to work properly!
-  mic.start();
+    // users must manually enable their browser microphone for recording to work properly!
+    mic.start();
 
-  // create a sound recorder
-  recorder = new p5.SoundRecorder();
+    // create a sound recorder
+    recorder = new p5.SoundRecorder();
 
-  // connect the mic to the recorder
-  recorder.setInput(mic);
+    // connect the mic to the recorder
+    recorder.setInput(mic);
 
-  // create an empty sound file that we will use to playback the recording
-  soundFile = new p5.SoundFile();
+    // create an empty sound file that we will use to playback the recording
+    soundFile = new p5.SoundFile();
 }
 
 function mousePressed() {
-  // use the '.enabled' boolean to make sure user enabled the mic (otherwise we'd record silence)
-  if (state === 0 && mic.enabled) {
+    background(255);
+    // use the '.enabled' boolean to make sure user enabled the mic (otherwise we'd record silence)
+    if (state === 0 && mic.enabled) {
 
-    // Tell recorder to record to a p5.SoundFile which we will use for playback
-    recorder.record(soundFile);
+        // Tell recorder to record to a p5.SoundFile which we will use for playback
+        recorder.record(soundFile);
 
-    background(255,0,0);
-    text('Recording now! Click to stop.', 20, 20);
-    state++;
-  }
+        fill(255, 20, 147);
+        noStroke();
+        ellipse(200, 200, 300, 300);
+        fill(0);
+        text('Recording now! Click to stop.', 20, 380);
+        state++;
+    } else if (state === 1) {
+        recorder.stop(); // stop recorder, and send the result to soundFile
 
-  else if (state === 1) {
-    recorder.stop(); // stop recorder, and send the result to soundFile
+        fill(20, 200, 0);
+        noStroke();
+        ellipse(200, 200, 300, 300);
+        fill(0);
+        text('Recording stopped. Click to play & save', 20, 380);
+        state++;
+    } else if (state === 2) {
+        soundFile.play(); // play the result!
+        fill(20, 200, 0);
+        noStroke();
+        ellipse(200, 200, 300, 300);
 
-    background(0,255,0);
-    text('Recording stopped. Click to play & save', 20, 20);
-    state++;
-  }
-
-  else if (state === 2) {
-    soundFile.play(); // play the result!
-    saveSound(soundFile, 'mySound.wav'); // save file
-    state++;
-  }
+        //saveSound(soundFile, 'mySound.wav'); // save file
+        state++;
+    }
 }
