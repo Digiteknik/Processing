d60c210a92752aa9b29b81f661e3f52217a6bd48
simmoe <simmoe@gmail.com>
2019-01-03 10:53:51 +0100

    Opdated libs

--- Processing/video_capture_p5/sketch.js
+++ Processing/video_capture_p5/sketch.js
@@ -1,23 +1,14 @@
-// Learning Processing
-// Daniel Shiffman
-// http://www.learningprocessing.com
-
-// Example 16-1: Display video
-
-var video;
+var capture;
 
 function setup() {
-  createCanvas(320, 240);
-  pixelDensity(1);
-  video = createCapture(VIDEO);
-  // The above function actually makes a separate video
-  // element on the page.  The line below hides it since we are
-  // drawing the video to the canvas
-  //video.hide();
+  createCanvas(390, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  //capture.hide();
 }
 
 function draw() {
-  background(0);
-  // Step 5. Display the video image.
-  image(video, 0, 0, width, height);
+  background(255);
+  image(capture, 0, 0, 320, 240);
+  filter('INVERT');
 }
