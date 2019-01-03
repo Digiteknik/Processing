371c50cf7e3b602c8e6c65748bb07d01eea29090
simmoe <simmoe@gmail.com>
2019-01-03 20:20:11 +0100

    update libs

--- Processing/fang_en_lyd_p5/sketch.js
+++ Processing/fang_en_lyd_p5/sketch.js
@@ -1,62 +1,27 @@
-var mic, recorder, soundFile;
-
-var state = 0; // mousePress will increment from Record, to Stop, to Play
+var mic
+var vol = 0
 
 function setup() {
-    createCanvas(400, 400);
-    fill(20, 200, 0);
-    noStroke();
-    ellipse(200, 200, 300, 300);
-    fill(0);
-    textSize(14);
-    text('Enable mic and click the mouse to begin recording', 20, 380);
-
-    // create an audio in
+    createCanvas(1200, 100);
+    // Create an Audio input
     mic = new p5.AudioIn();
-
-    // users must manually enable their browser microphone for recording to work properly!
+    // start the Audio Input.
+    // By default, it does not .connect() (to the computer speakers)
     mic.start();
-
-    // create a sound recorder
-    recorder = new p5.SoundRecorder();
-
-    // connect the mic to the recorder
-    recorder.setInput(mic);
-
-    // create an empty sound file that we will use to playback the recording
-    soundFile = new p5.SoundFile();
+    let d = createElement('div', 'Tryk på cirklen for at tænde mikrofonen...').style('font-family:arial;color:gray').position(20, 70);
 }
 
-function mousePressed() {
-    background(255);
-    // use the '.enabled' boolean to make sure user enabled the mic (otherwise we'd record silence)
-    if (state === 0 && mic.enabled) {
+function draw() {
+    // Get the overall volume (between 0 and 1.0)
+    var vol = mic.getLevel();
+    background(240);
+    fill('hotpink');
+    noStroke();
+    // Draw an ellipse with height based on volume
+    var h = map(vol, 0, 0.5, height, 0);
+    ellipse(width/2, h - 25, 50, 50);
+}
 
-        // Tell recorder to record to a p5.SoundFile which we will use for playback
-        recorder.record(soundFile);
-
-        fill(255, 20, 147);
-        noStroke();
-        ellipse(200, 200, 300, 300);
-        fill(0);
-        text('Recording now! Click to stop.', 20, 380);
-        state++;
-    } else if (state === 1) {
-        recorder.stop(); // stop recorder, and send the result to soundFile
-
-        fill(20, 200, 0);
-        noStroke();
-        ellipse(200, 200, 300, 300);
-        fill(0);
-        text('Recording stopped. Click to play & save', 20, 380);
-        state++;
-    } else if (state === 2) {
-        soundFile.play(); // play the result!
-        fill(20, 200, 0);
-        noStroke();
-        ellipse(200, 200, 300, 300);
-
-        //saveSound(soundFile, 'mySound.wav'); // save file
-        state++;
-    }
+function touchStarted() {
+    getAudioContext().resume()
 }
